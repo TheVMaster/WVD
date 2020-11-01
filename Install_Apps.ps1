248e9ed9beb06ff15626e2321c7ec022d89b6219
Helmer <helmer@zandbergenonline.nl>
2020-11-01 16:11:22 +0100

    Add files via upload

---
+++ Install_Apps.ps1
@@ -0,0 +1,24 @@
+
+#Script to setup golden image with Azure Image Builder
+
+
+#Create temp folder
+New-Item -Path 'C:\temp' -ItemType Directory -Force | Out-Null
+
+
+#Install VSCode
+Invoke-WebRequest -Uri 'https://go.microsoft.com/fwlink/?Linkid=852157' -OutFile 'c:\temp\VScode.exe'
+Invoke-Expression -Command 'c:\temp\VScode.exe /verysilent'
+
+#Start sleep
+Start-Sleep -Seconds 10
+
+#Start sleep
+Start-Sleep -Seconds 10
+
+
+#InstallTeamsMachinemode
+Invoke-WebRequest -Uri 'https://teams.microsoft.com/downloads/desktopurl?env=production&plat=windows&download=true&managedInstaller=true&arch=x64' -OutFile 'c:\temp\Teams.msi'
+Invoke-Expression -Command 'msiexec /i C:\temp\Teams.msi /quiet /l*v C:\temp\teamsinstall.log ALLUSER=1'
+Start-Sleep -Seconds 30
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run32 -Name Teams -PropertyType Binary -Value ([byte[]](0x01,0x00,0x00,0x00,0x1a,0x19,0xc3,0xb9,0x62,0x69,0xd5,0x01)) -Force
